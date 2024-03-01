1e6b80ecb48de10e3fc84389341d61ed54e37c42
IgorManin <106236581+IgorManin@users.noreply.github.com>
2024-03-01 22:42:17 +0300

    вынес карты на столе из компоненты Deck

--- src/components/GameContent/Deck.tsx
+++ src/components/GameContent/Deck.tsx
@@ -13,8 +13,19 @@ interface DeckProps {
   cardsOnTheTable: Card[] | null;
 }
 
+const cards = (cardsOnTheTable: Card[] | null) => {
+  return cardsOnTheTable?.map((card, index) => (
+    <div key={index}>
+      <Stack height={170} width={100} border={1} margin={2}>
+        {card?.suit} {card?.rank}
+      </Stack>
+    </div>
+  ));
+};
+
 export const Deck: React.FC<DeckProps> = ({ deck, cardsOnTheTable }) => {
   const dispatch = useDispatch();
+  const playingCards = cards(cardsOnTheTable);
 
   const takeCards = () => {
     dispatch(takeCardsFromTheTable({ playerType: 'player' }));
@@ -34,16 +45,7 @@ export const Deck: React.FC<DeckProps> = ({ deck, cardsOnTheTable }) => {
       height={230}
       width={800}
     >
-      <Stack>
-        {cardsOnTheTable &&
-          cardsOnTheTable?.map((card, index) => (
-            <div key={index}>
-              <Stack height={170} width={100} border={1} margin={2}>
-                {card?.suit} {card?.rank}
-              </Stack>
-            </div>
-          ))}
-      </Stack>
+      <Stack>{playingCards}</Stack>
 
       <Button variant="contained" onClick={takeCards}>
         Взять карты
